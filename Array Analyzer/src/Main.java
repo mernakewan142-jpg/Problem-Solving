5fee5e6a9aef7f98be330bb2bc537597a921143d
mernakewan142-jpg <mernakewan142@gmail.com>
2026-08-04 00:57:08 +0300

    Add Array Analyzer Solution

--- Array Analyzer/src/Main.java
+++ Array Analyzer/src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int size = input.nextInt();
+
+        int[] arr = new int[size];
+
+        int sum = 0;
+
+        System.out.println("Enter the elements:");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
+            sum += arr[i];
         }
+
+        int largest = arr[0];
+        int smallest = arr[0];
+
+        for (int i = 1; i < size; i++) {
+
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        double average = (double) sum / size;
+
+        System.out.println("Largest Element = " + largest);
+        System.out.println("Smallest Element = " + smallest);
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
     }
 }
