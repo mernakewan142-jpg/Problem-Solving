b0efcf3e789193e6a096889eeab768415fee9e50
mernakewan142-jpg <mernakewan142@gmail.com>
2026-08-04 00:55:42 +0300

    Add Classroom Statistics Solution

--- Classroom Statistics/src/Main.java
+++ Classroom Statistics/src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner input = new Scanner(System.in);
+
+        int highest = 0;
+        int lowest = 100;
+        int sum = 0;
+        int passed = 0;
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.print("Enter mark " + i + ": ");
+            int mark = input.nextInt();
+
+            if (mark > highest) {
+                highest = mark;
+            }
+
+            if (mark < lowest) {
+                lowest = mark;
+            }
+
+            sum += mark;
+
+            if (mark >= 50) {
+                passed++;
+            }
         }
+
+        double average = (double) sum / 10;
+
+        System.out.println("Highest Mark = " + highest);
+        System.out.println("Lowest Mark = " + lowest);
+        System.out.println("Average = " + average);
+        System.out.println("Passed Students = " + passed);
     }
 }
