b95a5cdc7511799931bd843b2690dff4c6d9cbdd
mernakewan142-jpg <mernakewan142@gmail.com>
2026-08-04 01:10:38 +0300

    Add Temperature Monitor Solution

--- Temperature Monitor/src/Main.java
+++ Temperature Monitor/src/Main.java
@@ -1,15 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] temp = new int[7];
+
+        int sum = 0;
+
+        System.out.println("Enter temperatures:");
+
+        for (int i = 0; i < 7; i++) {
+            temp[i] = input.nextInt();
+            sum += temp[i];
         }
+
+        int highest = temp[0];
+        int lowest = temp[0];
+
+        for (int i = 1; i < 7; i++) {
+
+            if (temp[i] > highest) {
+                highest = temp[i];
+            }
+
+            if (temp[i] < lowest) {
+                lowest = temp[i];
+            }
+
+        }
+
+        double average = sum / 7.0;
+
+        int daysAboveAverage = 0;
+
+        for (int i = 0; i < 7; i++) {
+
+            if (temp[i] > average) {
+                daysAboveAverage++;
+            }
+
+        }
+
+        System.out.println("Highest Temperature = " + highest);
+        System.out.println("Lowest Temperature = " + lowest);
+        System.out.println("Weekly Average = " + average);
+        System.out.println("Days Above Average = " + daysAboveAverage);
+
     }
 }
