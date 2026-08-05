9c6860eab1ef5b64daee098459be0297f2282964
mernakewan142-jpg <mernakewan142@gmail.com>
2026-08-05 18:25:36 +0300

    Add student ranking system Solution

--- Student Ranking System/src/Main.java
+++ Student Ranking System/src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = input.nextInt();
+        input.nextLine();
+
+        String[] names = new String[n];
+        int[] marks = new int[n];
+
+        int sum = 0;
+        int passed = 0;
+
+        int highest = 0;
+        int lowest = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.print("Enter student name: ");
+            names[i] = input.nextLine();
+
+            System.out.print("Enter mark: ");
+            marks[i] = input.nextInt();
+            input.nextLine();
+
+            sum += marks[i];
+
+            if (marks[i] >= 60)
+                passed++;
+
+            if (i == 0) {
+
+                highest = 0;
+                lowest = 0;
+
+            } else {
+
+                if (marks[i] > marks[highest]) {
+                    highest = i;
+                }
+
+                if (marks[i] < marks[lowest]) {
+                    lowest = i;
+                }
+
+            }
+
         }
+
+        double average = (double) sum / n;
+
+        System.out.println("Top Student = " + names[highest]);
+        System.out.println("Highest Mark = " + marks[highest]);
+
+        System.out.println("Lowest Student = " + names[lowest]);
+        System.out.println("Lowest Mark = " + marks[lowest]);
+
+        System.out.println("Class Average = " + average);
+
+        System.out.println("Passed Students = " + passed);
+
     }
+
 }
