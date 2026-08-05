e34d0dd4406b35097da565e7db02e3ad75cc4ff3
mernakewan142-jpg <mernakewan142@gmail.com>
2026-08-05 18:19:29 +0300

    Add character frequency report Solution

--- Chracter Frequency Report/src/Main.java
+++ Chracter Frequency Report/src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = input.nextLine();
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char current = text.charAt(i);
+
+            boolean printed = false;
+
+            for (int j = 0; j < i; j++) {
+
+                if (current == text.charAt(j)) {
+                    printed = true;
+                    break;
+                }
+
+            }
+
+            if (!printed) {
+
+                int count = 0;
+
+                for (int k = 0; k < text.length(); k++) {
+
+                    if (current == text.charAt(k)) {
+                        count++;
+                    }
+
+                }
+
+                System.out.println(current + " : " + count);
+
+            }
+
         }
+
     }
+
 }
