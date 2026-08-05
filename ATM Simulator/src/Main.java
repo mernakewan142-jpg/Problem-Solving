828b6513b6d56180a058adf54d779465105fdbe5
mernakewan142-jpg <mernakewan142@gmail.com>
2026-08-05 19:21:29 +0300

    Add ATM simulator Solution

--- ATM Simulator/src/Main.java
+++ ATM Simulator/src/Main.java
@@ -1,15 +1,111 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    static Scanner input = new Scanner(System.in);
+
+    static double balance = 0;
+    static String history = "";
+
+    public static void main(String[] args) {
+
+        int choice;
+
+        do {
+
+            System.out.println("\n===== ATM MENU =====");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Transaction History");
+            System.out.println("5. Exit");
+            System.out.print("Choose: ");
+
+            choice = input.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    deposit();
+                    break;
+
+                case 2:
+                    withdraw();
+                    break;
+
+                case 3:
+                    checkBalance();
+                    break;
+
+                case 4:
+                    showHistory();
+                    break;
+
+                case 5:
+                    System.out.println("Thank you!");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice");
+
+            }
+
+        } while (choice != 5);
+
     }
+
+    public static void deposit() {
+
+        System.out.print("Enter amount: ");
+        double amount = input.nextDouble();
+
+        balance += amount;
+
+        history += "Deposit: +" + amount + "\n";
+
+        System.out.println("Deposit Successful.");
+
+    }
+
+    public static void withdraw() {
+
+        System.out.print("Enter amount: ");
+        double amount = input.nextDouble();
+
+        if (amount <= balance) {
+
+            balance -= amount;
+
+            history += "Withdraw: -" + amount + "\n";
+
+            System.out.println("Withdrawal Successful.");
+
+        } else {
+
+            System.out.println("Insufficient Balance");
+
+        }
+
+    }
+
+    public static void checkBalance() {
+
+        System.out.println("Current Balance = " + balance);
+
+    }
+
+    public static void showHistory() {
+
+        if (history.equals("")) {
+
+            System.out.println("No Transactions Yet.");
+
+        } else {
+
+            System.out.println(history);
+
+        }
+
+    }
+
 }
