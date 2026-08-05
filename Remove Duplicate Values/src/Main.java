535bfd97d5533710a10659129ebc937f77387ffe
mernakewan142-jpg <mernakewan142@gmail.com>
2026-08-05 17:18:39 +0300

    Add Remove Duplicate Values Solution

--- Remove Duplicate Values/src/Main.java
+++ Remove Duplicate Values/src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int size = input.nextInt();
+
+        int[] arr = new int[size];
+        int[] unique = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            arr[i] = input.nextInt();
         }
+
+        int count = 0;
+
+        for (int i = 0; i < size; i++) {
+
+            boolean found = false;
+
+            for (int j = 0; j < count; j++) {
+                if (arr[i] == unique[j]) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                unique[count] = arr[i];
+                count++;
+            }
+        }
+
+        System.out.print("Unique Values: ");
+
+        for (int i = 0; i < count; i++) {
+            System.out.print(unique[i] + " ");
+        }
+
     }
 }
